692871a4ee09354305e1040237e0595bdc63a000
aleksandr <drevs.aleksandr@gmail.com>
2025-12-16 14:55:10 +0300

    nav, step 1

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 import "/src/sass/style.scss";
+
+let openNav = document.querySelector('.main-header__open-nav');
+let mainNav = document.querySelector('.main-nav');
+let closeNav = document.querySelector('.main-nav__close');
+
+openNav.addEventListener('click', () => {
+    openNav.classList.add('visibtleButtonNav');
+    mainNav.classList.add('navVisible');
+});
+
+closeNav.addEventListener('click', () => {
+    openNav.classList.remove('visibtleButtonNav');
+    mainNav.classList.remove('navVisible');
+});
